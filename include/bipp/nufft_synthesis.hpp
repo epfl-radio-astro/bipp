6265e269f34cf03986f5568f24f1a42b79294288
Simon Frasch <simon.frasch@cscs.ch>
2023-03-01 10:21:05 +0100

    add documentation for options

--- include/bipp/nufft_synthesis.hpp
+++ include/bipp/nufft_synthesis.hpp
@@ -19,12 +19,23 @@ namespace bipp {
 /*! \endcond */
 
 struct Partition {
-
+  /**
+   * Automatic domain partition method setting.
+   */
   struct Auto {};
 
+  /**
+   * Disable domain partitioning.
+   */
   struct None {};
 
+  /**
+   * Use regular grid domain partitioning
+   */
   struct Grid {
+    /**
+     * Grid dimension.
+     */
     std::array<std::size_t, 3> dimensions = {1, 1, 1};
   };
 
@@ -32,35 +43,70 @@ struct Partition {
 };
 
 struct NufftSynthesisOptions {
+  /**
+   * The tolerance used when computing the NUFFT. Smaller value will increase accuracy but requires
+   * more operations.
+   */
   float tolerance = 0.001f;
 
+  /**
+   * The maximum number of collected datasets processed together. Larger number typically improves
+   * performance but requires more memory. Internal heuristic is used if unset.
+   */
   std::optional<std::size_t> collectGroupSize = std::nullopt;
 
+  /**
+   * The partition method used in the UVW domain. Partitioning decreases memory usage, but may come
+   * with a performance penalty.
+   */
   Partition localImagePartition = Partition{Partition::Auto()};
 
+  /**
+   * The partition method used in the image domain. Partitioning decreases memory usage, but may
+   * come with a performance penalty.
+   */
   Partition localUVWPartition = Partition{Partition::Auto()};
 
+  /**
+   * Set the tolerance.
+   *
+   * @param[in] tol Tolerance.
+   */
   inline auto set_tolerance(float tol) -> NufftSynthesisOptions& {
     tolerance = tol;
     return *this;
   }
 
+  /**
+   * Set the collection group size.
+   *
+   * @param[in] size Collection group size.
+   */
   inline auto set_collect_group_size(std::optional<std::size_t> size) -> NufftSynthesisOptions& {
     collectGroupSize = size;
     return *this;
   }
 
+  /**
+   * Set the partitioning method for the UVW domain.
+   *
+   * @param[in] p Partition method.
+   */
   inline auto set_local_image_partition(Partition p) -> NufftSynthesisOptions& {
     localImagePartition = std::move(p);
     return *this;
   }
 
+  /**
+   * Set the partitioning method for the image domain.
+   *
+   * @param[in] p Partition method.
+   */
   inline auto set_local_uvw_partition(Partition p) -> NufftSynthesisOptions& {
     localUVWPartition = std::move(p);
     return *this;
   }
 };
-
 
 template <typename T>
 class BIPP_EXPORT NufftSynthesis {
