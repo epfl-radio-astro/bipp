c53dfe40e61dfe27894f92399118cfd1bc97f90a
Arpan Das <arpan.das@epfl.ch>
2023-06-22 13:04:54 +0200

    unwanted beams dropped

--- python/bipp/newmeasurementset.py
+++ python/bipp/newmeasurementset.py
@@ -212,25 +212,226 @@ class MeasurementSet:
             # We only want XX and YY correlations
             data = np.average(data[:, :, [0, 3]], axis=2)[:, channel_id]
             data_flag = np.any(data_flag[:, :, [0, 3]], axis=2)[:, channel_id]
-
-            # Set broken visibilities to 0
             data[data_flag] = 0
-            
-            working_beam = np.unique(beam_id_0)
-            
+
+            # DataFrame description of visibility data.
+            # Each column represents a different channel.
+            S_full_idx = pd.MultiIndex.from_arrays((beam_id_0, beam_id_1), names=("B_0", "B_1"))
+            S_full = pd.DataFrame(data=data, columns=channel_id, index=S_full_idx)
+
+
+            # Drop rows of `S_full` corresponding to unwanted beams.
+            beam_id = np.unique(self.instrument._layout.index.get_level_values("STATION_ID"))
+            N_beam = len(beam_id)
+            i, j = np.triu_indices(N_beam, k=0)
+            wanted_index = pd.MultiIndex.from_arrays((beam_id[i], beam_id[j]), names=("B_0", "B_1"))
+            index_to_drop = S_full_idx.difference(wanted_index)
+            S_trunc = S_full.drop(index=index_to_drop)
+            S = S_trunc.reset_index()
             for ch_id in channel_id:
-                matrix_data = data[:, ch_id]
+                matrix_data = S[ch_id]
 
-                matrix_data = np.ravel(matrix_data)
+                matrix_size = max(np.max(S.B_0), np.max(S.B_1)) + 1
 
-                matrix_size = max(np.max(beam_id_0), np.max(beam_id_1)) + 1
-
-                v = coo_matrix((matrix_data, (beam_id_0, beam_id_1)), shape=(matrix_size, matrix_size)).toarray()
+                v = coo_matrix((matrix_data, (S.B_0, S.B_1)), shape=(matrix_size, matrix_size)).toarray()
 
                 t = time.Time(sub_table.calc("MJD(TIME)")[0], format="mjd", scale="utc")
                 f = self.channels["FREQUENCY"][ch_id]
-            
-                visibility = vis.VisibilityMatrix(v, matrix_size)
-                
-                yield t, f visibility, working_beam
+
+                #visibility = vis.VisibilityMatrix(v, matrix_size)
+
+                yield t, f, S_trunc.index, v
+
+
+class LofarMeasurementSet(MeasurementSet):
+    """
+    LOw-Frequency ARray (LOFAR) Measurement Set reader.
+    """
+
+    @chk.check(
+        dict(
+            file_name=chk.is_instance(str),
+            N_station=chk.allow_None(chk.is_integer),
+            station_only=chk.is_boolean,
+        )
+    )
+    def __init__(self, file_name, N_station=None, station_only=False):
+        """
+        Parameters
+        ----------
+        file_name : str
+            Name of the MS file.
+        N_station : int
+            Number of stations to use. (Default = all)
+
+            Sometimes only a subset of an instrument’s stations are desired.
+            Setting `N_station` limits the number of stations to those that appear first when sorted
+            by STATION_ID.
+        station_only : bool
+            If :py:obj:`True`, model LOFAR stations as single-element antennas. (Default = False)
+        """
+        super().__init__(file_name)
+
+        if N_station is not None:
+            if N_station <= 0:
+                raise ValueError("Parameter[N_station] must be positive.")
+        self._N_station = N_station
+        self._station_only = station_only
+
+    @property
+    def instrument(self):
+        """
+        Returns
+        -------
+        :py:class:`~pypeline.phased_array.instrument.EarthBoundInstrumentGeometryBlock`
+            Instrument position computer.
+        """
+        if self._instrument is None:
+            # Following the LOFAR MS file specification from https://www.astron.nl/lofarwiki/lib/exe/fetch.php?media=public:documents:ms2_description_for_lofar_2.08.00.pdf,
+            # the special LOFAR_ANTENNA_FIELD sub-table must be used due to the hierarchical design
+            # of LOFAR.
+            # Some remarks on the required fields:
+            # - ANTENNA_ID: equivalent to STATION_ID field in `InstrumentGeometry.index[0]`.
+            # - POSITION: absolute station positions in ITRF coordinates.
+            #             This does not necessarily correspond to the station centroid.
+            # - ELEMENT_OFFSET: offset of each antenna in a station.
+            #                   When combined with POSITION, it gives the absolute antenna positions
+            #                   in ITRF.
+            # - ELEMENT_FLAG: True/False value for each (station, antenna, polarization) pair.
+            #                 If any of the polarization flags is True for a given antenna, then the
+            #                 antenna can be discarded from that station.
+            query = f"select ANTENNA_ID, POSITION, ELEMENT_OFFSET, ELEMENT_FLAG from {self._msf}::LOFAR_ANTENNA_FIELD"
+            table = ct.taql(query)
+
+            station_id = table.getcol("ANTENNA_ID")
+            station_mean = table.getcol("POSITION")
+            antenna_offset = table.getcol("ELEMENT_OFFSET")
+            antenna_flag = table.getcol("ELEMENT_FLAG")
+
+            # Form DataFrame that holds all antennas, then filter out flagged antennas.
+            N_station, N_antenna, _ = antenna_offset.shape
+            station_mean = np.reshape(station_mean, (N_station, 1, 3))
+            antenna_xyz = np.reshape(station_mean + antenna_offset, (N_station * N_antenna, 3))
+            antenna_flag = np.reshape(antenna_flag.any(axis=2), (N_station * N_antenna))
+
+            cfg_idx = pd.MultiIndex.from_product(
+                [station_id, range(N_antenna)], names=("STATION_ID", "ANTENNA_ID")
+            )
+            cfg = pd.DataFrame(data=antenna_xyz, columns=("X", "Y", "Z"), index=cfg_idx).loc[
+                ~antenna_flag
+            ]
+
+            # If in `station_only` mode, return centroid of each station only.
+            # Why do we not just use `station_mean` above? Because it arbitrarily
+            # points to some sub-antenna, not the station centroid.
+            if self._station_only:
+                cfg = cfg.groupby("STATION_ID").mean()
+                station_id = cfg.index.get_level_values("STATION_ID")
+                cfg.index = pd.MultiIndex.from_product(
+                    [station_id, [0]], names=["STATION_ID", "ANTENNA_ID"]
+                )
+
+            # Finally, only keep the stations that were specified in `__init__()`.
+            XYZ = instrument.InstrumentGeometry(xyz=cfg.values, ant_idx=cfg.index)
+            self._instrument = instrument.EarthBoundInstrumentGeometryBlock(XYZ, self._N_station)
+
+        return self._instrument
+
+    @property
+    def beamformer(self):
+        """
+        Each dataset has been beamformed in a specific way.
+        This property outputs the correct beamformer to compute the beamforming weights.
+
+        Returns
+        -------
+        :py:class:`~pypeline.phased_array.beamforming.MatchedBeamformerBlock`
+            Beamweight computer.
+        """
+        if self._beamformer is None:
+            # LOFAR uses Matched-Beamforming exclusively, with a single beam output per station.
+            XYZ = self.instrument._layout
+            beam_id = np.unique(XYZ.index.get_level_values("STATION_ID"))
+
+            direction = self.field_center
+            beam_config = [(_, _, direction) for _ in beam_id]
+            self._beamformer = beamforming.MatchedBeamformerBlock(beam_config)
+
+        return self._beamformer
+
+
+class MwaMeasurementSet(MeasurementSet):
+    """
+    Murchison Widefield Array (MWA) Measurement Set reader.
+    """
+
+    @chk.check("file_name", chk.is_instance(str))
+    def __init__(self, file_name):
+        """
+        Parameters
+        ----------
+        file_name : str
+            Name of the MS file.
+        """
+        super().__init__(file_name)
+
+    @property
+    def instrument(self):
+        """
+        Returns
+        -------
+        :py:class:`~pypeline.phased_array.instrument.EarthBoundInstrumentGeometryBlock`
+            Instrument position computer.
+        """
+        if self._instrument is None:
+            # Following the MS file specification from https://casa.nrao.edu/casadocs/casa-5.1.0/reference-material/measurement-set,
+            # the ANTENNA sub-table specifies the antenna geometry.
+            # Some remarks on the required fields:
+            # - POSITION: absolute station positions in ITRF coordinates.
+            # - ANTENNA_ID: equivalent to STATION_ID field `InstrumentGeometry.index[0]`
+            #               This field is NOT present in the ANTENNA sub-table, but is given
+            #               implicitly by its row-ordering.
+            #               In other words, the station corresponding to ANTENNA1=k in the MAIN
+            #               table is described by the k-th row of the ANTENNA sub-table.
+            query = f"select POSITION from {self._msf}::ANTENNA"
+            table = ct.taql(query)
+            station_mean = table.getcol("POSITION")
+
+            N_station = len(station_mean)
+            station_id = np.arange(N_station)
+            cfg_idx = pd.MultiIndex.from_product(
+                [station_id, [0]], names=("STATION_ID", "ANTENNA_ID")
+            )
+            cfg = pd.DataFrame(data=station_mean, columns=("X", "Y", "Z"), index=cfg_idx)
+
+            XYZ = instrument.InstrumentGeometry(xyz=cfg.values, ant_idx=cfg.index)
+
+            self._instrument = instrument.EarthBoundInstrumentGeometryBlock(XYZ)
+
+        return self._instrument
+
+    @property
+    def beamformer(self):
+        """
+        Each dataset has been beamformed in a specific way.
+        This property outputs the correct beamformer to compute the beamforming weights.
+
+        Returns
+        -------
+        :py:class:`~pypeline.phased_array.beamforming.MatchedBeamformerBlock`
+            Beamweight computer.
+        """
+        if self._beamformer is None:
+            # MWA does not do any beamforming.
+            # Given the single-antenna station model in MS files from MWA, this can be seen as
+            # Matched-Beamforming, with a single beam output per station.
+            XYZ = self.instrument._layout
+            beam_id = np.unique(XYZ.index.get_level_values("STATION_ID"))
+
+            direction = self.field_center
+            beam_config = [(_, _, direction) for _ in beam_id]
+            self._beamformer = beamforming.MatchedBeamformerBlock(beam_config)
+
+        return self._beamformer
+
 
