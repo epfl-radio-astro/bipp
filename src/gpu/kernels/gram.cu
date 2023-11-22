7ce8e7c78eef7f1cde53f45da020180d2cb9d683
Etienne Orliac <etienne.orliac@epfl.ch>
2023-11-22 09:54:57 +0100

    Fixing the Gram matrix computation (off-diagonal elements)

--- src/gpu/kernels/gram.cu
+++ src/gpu/kernels/gram.cu
@@ -12,18 +12,20 @@ namespace gpu {
 static __device__ __forceinline__ float calc_sqrt(float x) { return sqrtf(x); }
 static __device__ __forceinline__ double calc_sqrt(double x) { return sqrt(x); }
 
-static __device__ __forceinline__ float calc_sinc(float a, float x) {
-    return x ? sinf(a * x) / (float(3.14159265358979323846) * x) : float(1.0);
+static __device__ __forceinline__ float calc_pi_sinc(float x) {
+    constexpr auto pi = float(3.14159265358979323846);
+    return x ? sinf(pi * x) / (pi * x) : float(1.0);
 }
-
-static __device__ __forceinline__ double calc_sinc(double a, double x) {
-    return x ? sin(a * x) / (double(3.14159265358979323846) * x) : double(1.0);
+static __device__ __forceinline__ double calc_pi_sinc(double x) {
+    constexpr auto pi = double(3.14159265358979323846);
+    return x ? sin(pi * x) / (pi * x) : double(1.0);
 }
 
 template <typename T>
 static __global__ void gram_kernel(std::size_t n, const T* __restrict__ x, const T* __restrict__ y,
                                    const T* __restrict__ z, T wl,
                                    api::ComplexType<T>* __restrict__ g, std::size_t ldg) {
+
   for (std::size_t j = threadIdx.y + blockIdx.y * blockDim.y; j < n; j += gridDim.y * blockDim.y) {
     T x1 = x[j];
     T y1 = y[j];
@@ -35,7 +37,7 @@ static __global__ void gram_kernel(std::size_t n, const T* __restrict__ x, const
       T diffZ = z1 - z[i];
 
       T norm = calc_sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ);
-      g[i + j * ldg] = {calc_sinc(wl, norm), 0};
+      g[i + j * ldg] = { calc_pi_sinc(T(2) * norm / wl), 0};
     }
   }
 }
@@ -45,12 +47,9 @@ auto gram(Queue& q, std::size_t n, const T* x, const T* y, const T* z, T wl, api
           std::size_t ldg) -> void {
   const int blockSizeX = std::min<int>(16, q.device_prop().maxThreadsDim[0]);
   const int blockSizeY = std::min<int>(16, q.device_prop().maxThreadsDim[1]);
-
   const dim3 block(blockSizeX, blockSizeY, 1);
   const auto grid = kernel_launch_grid(q.device_prop(), {n, n, 1}, block);
-
-  api::launch_kernel(gram_kernel<T>, grid, block, 0, q.stream(), n, x, y, z,
-                     T(2 * 3.14159265358979323846 / wl), g, ldg);
+  api::launch_kernel(gram_kernel<T>, grid, block, 0, q.stream(), n, x, y, z, wl, g, ldg);
 }
 
 template auto gram<float>(Queue& q, std::size_t n, const float* x, const float* y, const float* z,
--- src/host/gram_matrix.cpp
+++ src/host/gram_matrix.cpp
@@ -20,8 +20,9 @@ namespace bipp {
 namespace host {
 
 template <typename T>
-static T calc_sinc(T a, T x) {
-  return x ? std::sin(a * x) / (T(3.14159265358979323846) * x) : T(1);
+static T calc_pi_sinc(T x) {
+  constexpr auto pi = T(3.14159265358979323846);
+  return x ? std::sin(pi * x) / (pi * x) : T(1);
 }
 
 template <typename T>
@@ -34,15 +35,15 @@ auto gram_matrix(ContextInternal& ctx, std::size_t m, std::size_t n, const std::
   auto x = xyz;
   auto y = xyz + ldxyz;
   auto z = xyz + 2 * ldxyz;
-  T sincScale = 2 * 3.14159265358979323846 / wl;
+  
   for (std::size_t i = 0; i < m; ++i) {
-    basePtr[i * m + i] = 1;
+    basePtr[i * m + i] = T(1);
     for (std::size_t j = i + 1; j < m; ++j) {
       auto diffX = x[i] - x[j];
       auto diffY = y[i] - y[j];
       auto diffZ = z[i] - z[j];
       auto norm = std::sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ);
-      basePtr[i * m + j] = calc_sinc(sincScale, norm);
+      basePtr[i * m + j] = calc_pi_sinc(T(2) * norm / wl);
     }
   }
 
