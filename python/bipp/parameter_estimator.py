14bae5c80f87cf089502ed7a6724329f7f5a6d7c
Simon Frasch <simon.frasch@cscs.ch>
2024-01-31 20:04:18 +0100

    reduced parameter estimator

--- python/bipp/parameter_estimator.py
+++ python/bipp/parameter_estimator.py
@@ -26,6 +26,7 @@ import sklearn.cluster as skcl
 
 import bipp.statistics as vis
 import bipp.gram as gr
+import bipp.filter
 import bipp.pybipp
 
 
@@ -62,37 +63,6 @@ def centroid_to_intervals(centroid):
 
 
 class ParameterEstimator:
-    """
-    Top-level public interface of Bipp parameter estimators.
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    def collect(self, *args, **kwargs):
-        """
-        Ingest data to internal queue for inference.
-
-        Args
-            \*args
-                Positional arguments.
-            \*\*kwargs
-                Keyword arguments.
-        """
-        raise NotImplementedError
-
-    def infer_parameters(self):
-        """
-        Estimate parameters given ingested data.
-
-        Returns
-            tuple
-                Parameters as defined by subclasses.
-        """
-        raise NotImplementedError
-
-
-class IntensityFieldParameterEstimator(ParameterEstimator):
     """
     Parameter estimator for computing intensity fields.
 
@@ -141,6 +111,8 @@ class IntensityFieldParameterEstimator(ParameterEstimator):
         self._inferred = False
 
     def num_level(self):
+        if not self._inferred:
+            self.infer_parameters()
         if len(self._intervals) == 0:
             return 0
         return self._intervals.shape[0]
@@ -148,93 +120,27 @@ class IntensityFieldParameterEstimator(ParameterEstimator):
     def __call__(self, level, D):
         if not self._inferred:
             self.infer_parameters()
+        if level > self._intervals.shape[1]:
+            raise ValueError("ParameterEstimator: level index out of bounds")
+
         D *= (D>=self._intervals[level, 0]) * (D<=self._intervals[level,1])
-        if self._N_eig < len(D):
-            D[0:-self._N_eig] = 0
         return D
 
     def infer_parameters(self):
         """
         Estimate parameters given ingested data.
 
-        Returns
-            N_eig : int
-                Number of eigenpairs to use.
-
         cluster_intervals : :py:class:`~numpy.ndarray`
-            (N_level,2) intensity field cluster intervals.
+            (N_level,2) intensity field intervals to select eigenvalues for each level.
         """
+        if len(self._d_all) == 0:
+            return 0, np.empty((0, 2))
+
         D_all = np.concatenate(self._d_all)
         kmeans = skcl.KMeans(n_clusters=self._N_level).fit(np.log(D_all).reshape(-1, 1))
 
-        # For extremely small telescopes or datasets that are mostly 'broken', we can have (N_eig < N_level).
-        # In this case we have two options: (N_level = N_eig) or (N_eig = N_level).
-        # In the former case, the user's choice of N_level is not respected and subsequent code written by the user
-        # could break due to a false assumption. In the latter case, we modify N_eig to match the user's choice.
-        # This has the disadvantage of increasing the computational load of Bipp, but as the N_eig energy levels
-        # are clustered together anyway, the trailing energy levels will be (close to) all-0 and can be discarded
-        # on inspection.
-        N_eig = max(int(np.ceil(len(D_all) / len(self._d_all))), self._N_level)
         cluster_centroid = np.sort(np.exp(kmeans.cluster_centers_)[:, 0])[::-1]
 
         self._inferred = True
-        self._N_eig = N_eig
         self._intervals = centroid_to_intervals(cluster_centroid)
-        return N_eig, self._intervals
-
-
-class SensitivityFieldParameterEstimator(ParameterEstimator):
-    """
-    Parameter estimator for computing sensitivity fields.
-
-    Args
-        sigma : float
-            Normalized energy ratio for fPCA decomposition.
-        ctx: :py:class:`~bipp.Context`
-            Bipp context. If provided, will use bipp module for computation.
-    """
-
-    def __init__(self, sigma, ctx):
-        super().__init__()
-
-        if not (0 < sigma <= 1):
-            raise ValueError("Parameter[sigma] must lie in (0,1].")
-        self._sigma = sigma
-
-        # Collected data.
-        self._grams = []
-        self._ctx = ctx
-
-    def collect(self, G):
-        """
-        Ingest data to internal queue for inference.
-
-        Args
-            G : :py:class:`~bipp.phased_array.bipp.gram.GramMatrix`
-                (N_beam, N_beam) gram matrix.
-        """
-        self._grams.append(G)
-
-    def infer_parameters(self):
-        """
-        Estimate parameters given ingested data.
-
-        Returns
-            N_eig : int
-                Number of eigenpairs to use.
-        """
-        N_data = len(self._grams)
-        N_beam = N_eig_max = self._grams[0].shape[0]
-
-        D_all = np.zeros((N_data, N_eig_max))
-        for i, G in enumerate(self._grams):
-            # Functional PCA
-            _, D, _ = bipp.pybipp.eigh(self._ctx, G.data.shape[0], G.data)
-            idx = np.clip(np.cumsum(D) / np.sum(D), 0, 1) <= self._sigma
-            D = D[idx]
-            D_all[i, : len(D)] = D
-
-        D_all = D_all[D_all.nonzero()]
-
-        N_eig = int(np.ceil(len(D_all) / N_data))
-        return N_eig
+        return self._intervals
