447b639dcd12ee0d9fa55e22ba4a8de20108d366
Simon Frasch <simon.frasch@cscs.ch>
2024-03-12 11:29:13 +0100

    pip mpi
    fix test compilation
    
    missing script

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ bipp_gpu = str(os.getenv("BIPP_GPU", "OFF"))
 bipp_umpire = str(os.getenv("BIPP_UMPIRE", "OFF"))
 bipp_omp = str(os.getenv("BIPP_OMP", "ON"))
 bipp_vc = str(os.getenv("BIPP_VC", "OFF"))
+bipp_mpi = str(os.getenv("BIPP_MPI", "OFF"))
 bipp_magma = str(os.getenv("BIPP_MAGMA", "OFF"))
 bipp_cmake_args = str(os.getenv("BIPP_CMAKE_ARGS", ""))
 bipp_cmake_args_list = shlex.split(bipp_cmake_args) if bipp_cmake_args else []
@@ -33,6 +34,7 @@ setup(
         "-DBIPP_GPU=" + bipp_gpu,
         "-DBIPP_UMPIRE=" + bipp_umpire,
         "-DBIPP_OMP=" + bipp_omp,
+        "-DBIPP_MPI=" + bipp_mpi,
         "-DBIPP_VC=" + bipp_vc,
         "-DBIPP_MAGMA=" + bipp_magma,
         "-DBIPP_BUNDLED_LIBS=ON",
