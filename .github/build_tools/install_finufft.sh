05dd381b93d45aa1df5f8563336ed442d8968b11
Simon Frasch <simon.frasch@cscs.ch>
2024-06-17 15:52:42 +0200

    fix deps

--- .github/build_tools/install_finufft.sh
+++ .github/build_tools/install_finufft.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-yum install -y openblas-openmp fftw-devel wget
 cd ${HOME}
 wget --quiet https://github.com/flatironinstitute/finufft/archive/refs/tags/v2.2.0.tar.gz
 tar -xzvf v2.2.0.tar.gz
