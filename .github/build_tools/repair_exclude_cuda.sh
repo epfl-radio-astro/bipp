20a2bf4e032a1f3e649152b9372b35a1fec171b8
Simon Frasch <simon.frasch@cscs.ch>
2024-06-17 13:16:43 +0200

    remove file type

--- .github/build_tools/repair_exclude_cuda.sh
+++ .github/build_tools/repair_exclude_cuda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 options=""
-for line in $(find /usr/local/cuda/lib64/ -name "lib*.so.*" -type f -printf "%f\n");
+for line in $(find /usr/local/cuda/lib64/ -name "lib*.so.*" -printf "%f\n");
 do
     options="$options --exclude $line"
 done
