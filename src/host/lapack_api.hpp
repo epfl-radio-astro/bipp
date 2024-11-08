6f1ade64aefea7da0865875bf56bff8d0b0e9caa
Etienne Orliac <etienne.orliac@epfl.ch>
2024-11-08 12:14:53 +0100

    Revert "Default to 1 thread in lapacke calls to avoid drastic performance issue in the case BIPP is linked to OpenBLAS built with openmp support"
    This reverts commit 334cb2d5944b1f3ccb4c321fc230798874e5ce10.

--- src/host/lapack_api.hpp
+++ src/host/lapack_api.hpp
@@ -2,9 +2,6 @@
 
 #include <complex>
 #include <vector>
-#include <iostream>
-#include <omp.h>
-
 
 #include "bipp/config.h"
 #include "bipp/exceptions.hpp"
@@ -70,28 +67,10 @@ inline auto dlamch(char cmach) -> double {
 #endif
 }
 
-inline auto set_omp_num_threads() -> int {
-  int env_omp_num_threads = 1;
-  if (const char* env_omp_num_threads_ = std::getenv("OMP_NUM_THREADS")) {
-      env_omp_num_threads = atoi(env_omp_num_threads_);
-  }
-  int lapack_nt = 1;
-  if (const char* lapack_nt_ = std::getenv("BIPP_LAPACK_C_NUM_THREADS")) {
-    const int lnt_tmp = atoi(lapack_nt_);
-    if (lnt_tmp > 0) lapack_nt = lnt_tmp;
-  }
-  //std::cout << "@@@@  lapack_nt " << lapack_nt << ", env_omp_num_threads " << env_omp_num_threads << '\n';
-  omp_set_num_threads(lapack_nt);
-
-  return env_omp_num_threads;
-}
-
 inline auto eigh_solve(LapackeLayout matrixLayout, char jobz, char uplo, int n,
                        std::complex<float>* a, int lda, float* w) -> void {
 #ifdef BIPP_LAPACK_C
-  auto env_omp_num_threads = set_omp_num_threads();
   const auto info = LAPACKE_cheev(static_cast<int>(matrixLayout), jobz, uplo, n, a, lda, w);
-  omp_set_num_threads(env_omp_num_threads);
 #else
   int info = 0;
   int lwork = -1;
@@ -112,9 +91,7 @@ inline auto eigh_solve(LapackeLayout matrixLayout, char jobz, char uplo, int n,
 inline auto eigh_solve(LapackeLayout matrixLayout, char jobz, char uplo, int n,
                        std::complex<double>* a, int lda, double* w) -> void {
 #ifdef BIPP_LAPACK_C
-  auto env_omp_num_threads = set_omp_num_threads();
   const auto info = LAPACKE_zheev(static_cast<int>(matrixLayout), jobz, uplo, n, a, lda, w);
-  omp_set_num_threads(env_omp_num_threads);
 #else
   int info = 0;
   int lwork = -1;
@@ -136,9 +113,7 @@ inline auto eigh_solve(LapackeLayout matrixLayout, int itype, char jobz, char up
                        std::complex<float>* a, int lda, std::complex<float>* b, int ldb, float* w)
     -> void {
 #ifdef BIPP_LAPACK_C
-  auto env_omp_num_threads = set_omp_num_threads();
   const auto info = LAPACKE_chegv(static_cast<int>(matrixLayout), itype, jobz, uplo, n, a, lda, b, ldb, w);
-  omp_set_num_threads(env_omp_num_threads);
 #else
   int info = 0;
   int lwork = -1;
@@ -162,9 +137,7 @@ inline auto eigh_solve(LapackeLayout matrixLayout, int itype, char jobz, char up
                        std::complex<double>* a, int lda, std::complex<double>* b, int ldb, double* w)
     -> void {
 #ifdef BIPP_LAPACK_C
-  auto env_omp_num_threads = set_omp_num_threads();
   const auto info = LAPACKE_zhegv(static_cast<int>(matrixLayout), itype, jobz, uplo, n, a, lda, b, ldb, w);
-  omp_set_num_threads(env_omp_num_threads);
 #else
   int info = 0;
   int lwork = -1;
