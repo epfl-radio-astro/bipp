c9048ff8f103f71404c6f135a8f34bd8a0755199
Simon Frasch <simon.frasch@cscs.ch>
2025-06-10 18:02:49 +0200

    adapt wheel scripts

--- .github/build_tools/install_finufft.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ${HOME}
-wget --quiet https://github.com/flatironinstitute/finufft/archive/refs/tags/v2.2.0.tar.gz
-tar -xzvf v2.2.0.tar.gz
-cd finufft-2.2.0 && mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DFINUFFT_ARCH_FLAGS=""
-make install -j
-
