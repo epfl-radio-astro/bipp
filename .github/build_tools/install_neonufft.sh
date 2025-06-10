e5980dbb8f48372fc0a1958f1d277e8cddebc4b8
Simon Frasch <simon.frasch@cscs.ch>
2025-06-10 21:52:47 +0200

    build static lib for wheel

--- .github/build_tools/install_neonufft.sh
+++ .github/build_tools/install_neonufft.sh
@@ -2,6 +2,6 @@
 cd ${HOME}
 git clone https://github.com/epfl-radio-astro/neonufft.git
 cd neonufft && mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DNEONUFFT_THREADING=NATIVE
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DNEONUFFT_THREADING=NATIVE -DBUILD_SHARED_LIBS=OFF
 make install -j
 
