964dfff71696e341c509ebd20cb5b222e0f37630
Simon Frasch <simon.frasch@cscs.ch>
2025-06-11 12:37:15 +0200

    use openmp instead of native threading

--- .github/build_tools/install_neonufft.sh
+++ .github/build_tools/install_neonufft.sh
@@ -2,6 +2,6 @@
 cd ${HOME}
 git clone https://github.com/epfl-radio-astro/neonufft.git
 cd neonufft && mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DNEONUFFT_THREADING=NATIVE -DBUILD_SHARED_LIBS=OFF -DHWY_ENABLE_INSTALL=ON
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DNEONUFFT_THREADING=OPENMP -DBUILD_SHARED_LIBS=OFF -DHWY_ENABLE_INSTALL=ON
 make install -j
 
