1ce4747c007648873f744d3a68d4a09017d8aabf
Simon Frasch <simon.frasch@cscs.ch>
2025-06-12 11:17:46 +0200

    fewer build threads

--- .github/build_tools/install_neonufft.sh
+++ .github/build_tools/install_neonufft.sh
@@ -3,5 +3,5 @@ cd ${HOME}
 git clone https://github.com/epfl-radio-astro/neonufft.git
 cd neonufft && mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DNEONUFFT_THREADING=OPENMP -DBUILD_SHARED_LIBS=OFF -DHWY_ENABLE_INSTALL=ON
-make install -j
+make install -j4
 
