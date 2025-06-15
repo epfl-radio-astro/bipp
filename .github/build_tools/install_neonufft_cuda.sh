4ee17b43bfeeec754c87d269cd8fd1c2dd8cd6e3
Simon Frasch <simon.frasch@cscs.ch>
2025-06-15 18:37:21 +0200

    Fix static build (#41)

--- .github/build_tools/install_neonufft_cuda.sh
+++ .github/build_tools/install_neonufft_cuda.sh
@@ -4,5 +4,5 @@ cd ${HOME}
 git clone https://github.com/epfl-radio-astro/neonufft.git
 cd neonufft && mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DNEONUFFT_THREADING=OPENMP -DCMAKE_CUDA_ARCHITECTURES=${CUDAARCHS} -DNEONUFFT_GPU=CUDA -DBUILD_SHARED_LIBS=OFF -DHWY_ENABLE_INSTALL=ON
-make install -j
+make install -j4
 
