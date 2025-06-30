Handling LOFAR uvw sign bug, see https://wsclean.readthedocs.io/en/latest/chgcentre.html#a-lofar-bug

python/bipp/apps/create_dataset.py
@@ -85,7 +85,6 @@ def create_dataset(args):
             wl = constants.speed_of_light / f.to_value(u.Hz)
             XYZ = ms.instrument(t)
             W = ms.beamformer(XYZ, wl)
-            #  S, W = measurement_set.filter_data(S, W)
 
             UVW_baselines_t = ms.instrument.baselines(
                 t, uvw=True, field_center=ms.field_center
@@ -95,7 +94,12 @@ def create_dataset(args):
             if np.allclose(S.data, np.zeros(S.data.shape)):
                 continue
 
-            v, d, scale = bipp.eigh_gram(wl, S.data, W.data, XYZ.data)
+            # See https://wsclean.readthedocs.io/en/latest/chgcentre.html#a-lofar-bug
+            if args.telescope.lower() == "lofar":
+                v, d, scale = bipp.eigh_gram(wl, S.data.conj(), W.data, XYZ.data)
+            else:
+                v, d, scale = bipp.eigh_gram(wl, S.data, W.data, XYZ.data)
+
             dataset.write(t.value, wl, scale, v, d, uvw)
             n_written += 1
 